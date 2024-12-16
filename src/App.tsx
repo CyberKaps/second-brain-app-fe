548217ea05087c88c21e028769b6071b7dffbc4e
Kalpesh Borde <bordekalpesh@gmail.com>
2024-12-16 14:01:52 +0530

    added buttons

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Button } from './components/ui/Button'
+import { PlusIcon } from './icons/PlusIcon'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,7 +11,10 @@ function App() {
   return (
     <>
      <div>
-      hj
+      <Button startIcon={<PlusIcon />} size='sm' varient='primary' text="Share" />
+      <Button startIcon={<PlusIcon />} size="md" varient='primary' text="Add Content" />
+      <Button startIcon={<PlusIcon />} size="lg" varient='secondary' text="Add Content" />
+
      </div>
     </>
   )
--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -9,10 +9,23 @@ export interface ButtonProps {
  
 }
 
+const varientStyles = {
+    "primary": "bg-purple-600 text-white",
+    "secondary": "bg-purple-300 text-purple-500"
+
+}
+
+const sizeStyles = {
+    "sm": "py-1 px-2",
+    "md": "py-2 px-4",
+    "lg": "py-4 px-6"
+}
+
+const defaultStyles = "rounded-md flex"
 
 export const Button = (props: ButtonProps) => {
 
-    return <button></button>
+    return <button className={`${varientStyles[props.varient]} ${defaultStyles} ${sizeStyles[props.size]}`}>{props.startIcon ? <div className="pr-2">{props.startIcon}</div> : null }{props.text} {props.endIcon}</button>
 
 }
 
